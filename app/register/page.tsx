819aa8d291875f29c652f91db67eddca95c92b5d
Jai892289 <gjai4341@gmail.com>
2026-04-03 13:36:38 +0530

    comment added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,7 @@
 export default function RegisterPage() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Register Page New</h1>
+      <h1 className="text-2xl font-bold">Register Page New 123</h1>
     </div>
   );
 }
