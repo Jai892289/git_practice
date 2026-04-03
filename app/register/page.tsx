e564f7ebd3c760aca5e7cda16abe1b6cff3f5ae1
Jai892289 <gjai4341@gmail.com>
2026-04-03 13:55:59 +0530

    added changes

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,11 @@ export default function RegisterPage() {
   return (
     <div className="h-screen flex items-center justify-center">
       <h1 className="text-2xl font-bold">Register Page New 123</h1>
+      <h1 className="text-2xl font-bold">Register Page New 32</h1>
+      <h1 className="text-2xl font-bold">Register Page New 32</h1>
+      <h1 className="text-2xl font-bold">Register Page New 5</h1>
+      <h1 className="text-2xl font-bold">Register Page New 44</h1>
+      <h1 className="text-2xl font-bold">Register Page New 123</h1>
     </div>
   );
 }
